c79d35079b1ce6d446028a33bdaaeede947350ee
Vaibhav <86156611+imvbh@users.noreply.github.com>
2024-08-28 19:00:56 +0530

    added list and buttons

--- lib/pages/note_edit_page.dart
+++ lib/pages/note_edit_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:minimal_notes_app/models/note_database.dart';
 import 'package:minimal_notes_app/models/note.dart';
+import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 
-class NoteEditPage extends StatelessWidget {
+class NoteEditPage extends StatefulWidget {
   final TextEditingController titleController;
   final TextEditingController descriptionController;
   final Note? note;
@@ -14,6 +18,13 @@ class NoteEditPage extends StatelessWidget {
     required this.descriptionController,
     this.note,
   }) : super(key: key);
+
+  @override
+  _NoteEditPageState createState() => _NoteEditPageState();
+}
+
+class _NoteEditPageState extends State<NoteEditPage> {
+  int _formattingStyle = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -29,22 +40,22 @@ class NoteEditPage extends StatelessWidget {
                 backgroundColor: Theme.of(context).colorScheme.primary,
               ),
               onPressed: () {
-                if (note == null) {
+                if (widget.note == null) {
                   context.read<NoteDatabase>().addNote(
-                        titleController.text,
-                        descriptionController.text,
+                        widget.titleController.text,
+                        widget.descriptionController.text,
                       );
                 } else {
                   context.read<NoteDatabase>().updateNote(
-                        note!.id,
-                        titleController.text,
-                        descriptionController.text,
+                        widget.note!.id,
+                        widget.titleController.text,
+                        widget.descriptionController.text,
                       );
                 }
                 Navigator.pop(context);
               },
               child: Text(
-                note == null ? 'Save' : 'Update',
+                widget.note == null ? 'Save' : 'Update',
                 style: TextStyle(
                     color: Theme.of(context).colorScheme.inversePrimary,
                     fontWeight: FontWeight.bold),
@@ -52,7 +63,10 @@ class NoteEditPage extends StatelessWidget {
             ),
           )
         ],
-        title: Text(note == null ? 'New Note' : 'Update Note',style: const TextStyle(fontWeight: FontWeight.bold),),
+        title: Text(
+          widget.note == null ? 'New Note' : 'Update Note',
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
         elevation: 0,
         backgroundColor: Theme.of(context).colorScheme.surface,
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -64,12 +78,11 @@ class NoteEditPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Padding(
-                
                 padding: const EdgeInsets.only(left: 4.0, right: 4.0),
                 child: TextField(
                   style: const TextStyle(
                       fontWeight: FontWeight.bold, fontSize: 25),
-                  controller: titleController,
+                  controller: widget.titleController,
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
                   cursorColor: Theme.of(context).colorScheme.inversePrimary,
@@ -88,7 +101,7 @@ class NoteEditPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(left: 4.0, right: 4.0),
                 child: TextField(
-                  controller: descriptionController,
+                  controller: widget.descriptionController,
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
                   cursorColor: Theme.of(context).colorScheme.inversePrimary,
@@ -105,10 +118,180 @@ class NoteEditPage extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 20),
+              // Add formatting options
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // IconButton(
+                  //   icon: Icon(Icons.format_bold),
+                  //   onPressed: () {
+                  //     final text = widget.descriptionController.text;
+                  //     final selection = widget.descriptionController.selection;
+
+                  //     if (text
+                  //             .substring(selection.start, selection.end)
+                  //             .startsWith('**') &&
+                  //         text
+                  //             .substring(selection.start, selection.end)
+                  //             .endsWith('**')) {
+                  //       final newText = text.substring(0, selection.start) +
+                  //           text.substring(
+                  //               selection.start + 2, selection.end - 2) +
+                  //           text.substring(selection.end);
+                  //       widget.descriptionController.text = newText;
+                  //       widget.descriptionController.selection = TextSelection(
+                  //           baseOffset: selection.start,
+                  //           extentOffset: selection.end - 4);
+                  //     } else {
+                  //       final newText = text.substring(0, selection.start) +
+                  //           '**' +
+                  //           text.substring(selection.start, selection.end) +
+                  //           '**' +
+                  //           text.substring(selection.end);
+                  //       widget.descriptionController.text = newText;
+                  //       widget.descriptionController.selection = TextSelection(
+                  //           baseOffset: selection.start + 2,
+                  //           extentOffset: selection.end + 2);
+                  //     }
+                  //   },
+                  // ),
+                  // IconButton(
+                  //   icon: Icon(Icons.format_italic),
+                  //   onPressed: () {
+                  //     final text = widget.descriptionController.text;
+                  //     final selection = widget.descriptionController.selection;
+
+                  //     if (text
+                  //             .substring(selection.start, selection.end)
+                  //             .startsWith('*') &&
+                  //         text
+                  //             .substring(selection.start, selection.end)
+                  //             .endsWith('*')) {
+                  //       final newText = text.substring(0, selection.start) +
+                  //           text.substring(
+                  //               selection.start + 1, selection.end - 1) +
+                  //           text.substring(selection.end);
+                  //       widget.descriptionController.text = newText;
+                  //       widget.descriptionController.selection = TextSelection(
+                  //           baseOffset: selection.start,
+                  //           extentOffset: selection.end - 2);
+                  //     } else {
+                  //       final newText = text.substring(0, selection.start) +
+                  //           '*' +
+                  //           text.substring(selection.start, selection.end) +
+                  //           '*' +
+                  //           text.substring(selection.end);
+                  //       widget.descriptionController.text = newText;
+                  //       widget.descriptionController.selection = TextSelection(
+                  //           baseOffset: selection.start + 1,
+                  //           extentOffset: selection.end + 1);
+                  //     }
+                  //   },
+                  // ),
+                  IconButton(
+                    icon: Icon(Icons.list),
+                    onPressed: () {
+                      setState(() {
+                        _formattingStyle = (_formattingStyle + 1) % 3;
+                      });
+
+                      String text = widget.descriptionController.text;
+
+                      List<String> lines = text.split('\n');
+
+                      switch (_formattingStyle) {
+                        case 0: // plain text
+                          text = lines
+                              .map((line) =>
+                                  line.replaceAll(RegExp(r'^- |^\d+\. '), ''))
+                              .join('\n');
+                          break;
+                        case 1: // point list
+                          text = lines
+                              .map((line) =>
+                                  '- ' +
+                                  line.replaceAll(RegExp(r'^- |^\d+\. '), ''))
+                              .join('\n');
+                          break;
+                        case 2: // numbered list
+                          text = lines
+                              .asMap()
+                              .entries
+                              .map((entry) =>
+                                  '${entry.key + 1}. ' +
+                                  entry.value
+                                      .replaceAll(RegExp(r'^- |^\d+\. '), ''))
+                              .join('\n');
+                          break;
+                      }
+
+                      widget.descriptionController.text = text;
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.image),
+                    onPressed: () {
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        _requestPermissionAndPickImage();
+                      });
+                    },
+                  ),
+
+                  IconButton(
+                    icon: Icon(Icons.mic),
+                    onPressed: () {
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        _requestPermissionAndRecordAudio();
+                      });
+                    },
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future<void> _requestPermissionAndPickImage() async {
+    var status = await Permission.storage.status;
+    if (status.isDenied) {
+      await Permission.storage.request();
+      status = await Permission.storage.status;
+    }
+    if (status.isGranted) {
+      final ImagePicker _picker = ImagePicker();
+      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        final File file = File(image.path);
+        // Store the image with the description text
+        widget.descriptionController.text += '\n[Image: ${file.path}]';
+      }
+    } else {
+      print('Storage permission denied');
+    }
+  }
+
+  Future<void> _requestPermissionAndRecordAudio() async {
+    var status = await Permission.microphone.status;
+    if (status.isDenied) {
+      await Permission.microphone.request();
+      status = await Permission.microphone.status;
+    }
+    if (status.isGranted) {
+      final FlutterSoundRecorder _recorder = FlutterSoundRecorder();
+      await _recorder.openRecorder();
+      await _recorder.startRecorder();
+      // Record audio for a few seconds
+      await Future.delayed(const Duration(seconds: 5));
+      final String? path = await _recorder.stopRecorder();
+      if (path != null) {
+        // Store the audio with the description text
+        widget.descriptionController.text += '\n[Audio: $path]';
+      }
+    } else {
+      print('Microphone permission denied');
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import file_selector_macos
 import isar_flutter_libs
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
